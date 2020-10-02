a90dee51ee196b075691db4ccc211ba27ffd236d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-02 13:42:45 +0000

    Deploy SarthakGirotra/Todolist-react to github.com/SarthakGirotra/Todolist-react.git:gh-pages

---
+++ precache-manifest.4a65c134844a621375d7b5228a30538a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1241744c32e93a252f30a96e82bdbbe9",
+    "url": "/Todolist-react/index.html"
+  },
+  {
+    "revision": "1de8bb36dbbd08618bbd",
+    "url": "/Todolist-react/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "eb74151a68650731aa30",
+    "url": "/Todolist-react/static/css/main.f3b1cb3b.chunk.css"
+  },
+  {
+    "revision": "1de8bb36dbbd08618bbd",
+    "url": "/Todolist-react/static/js/2.adfc3059.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "/Todolist-react/static/js/2.adfc3059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb74151a68650731aa30",
+    "url": "/Todolist-react/static/js/main.7e2b83d1.chunk.js"
+  },
+  {
+    "revision": "a57663e887711b5c968a",
+    "url": "/Todolist-react/static/js/runtime-main.cac43f4d.js"
+  }
+]);
